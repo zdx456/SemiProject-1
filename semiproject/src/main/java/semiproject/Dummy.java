46f507d1a1c2adfde61ddea810b34165d7428833
zdx456 <zdx456@naver.com>
2022-05-03 09:57:59 +0900

    test

---
+++ semiproject/src/main/java/semiproject/Dummy.java
@@ -0,0 +1,5 @@
+package semiproject;
+
+public class Dummy {
+
+}
--- testproject/src/testproject/Test01.java
+++
@@ -1,8 +0,0 @@
-package testproject;
-
-public class Test01 {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
-
-}
