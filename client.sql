12db1d0b95a7d0efe673549dc5cd3cf527841e16
songhyeondo <99389064+songhyeondo@users.noreply.github.com>
2022-05-03 14:41:51 +0900

    Update client.sql

--- client.sql
+++ client.sql
@@ -1,8 +1,8 @@
 drop table client;
 create table client(
-client_id varchar2(20) primary key check(regexp_like(client_id, '^[a-z]{0,15}$')),
+client_id varchar2(20) primary key check(regexp_like(client_id, '^[a-z]{0,16}$')),
 client_pw varchar2(16) not null 
-check(regexp_like(client_pw, '^[a-z]{7,15}$') and regexp_like(client_pw, '[A-Z]+')
+check(regexp_like(client_pw, '^[a-z]{9,17}$') and regexp_like(client_pw, '[A-Z]+')
 			and regexp_like(client_pw, '[0-9]+') and regexp_like(client_pw, '[!@#$]+')),
 client_nick varchar2(30) not null unique check(regexp_like(client_pw, '[a-z]+') and regexp_like(client_pw, '[A-Z]+') and(client_nick, '^[ㄱ-ㅎ가-힣0-9]{1,10}$')),
 client_joindate date default sysdate not null,
