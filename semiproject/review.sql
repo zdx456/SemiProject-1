de8c2ecec349b471de45f3f2163358edf34e4d02
zdx456 <zdx456@naver.com>
2022-05-03 12:12:38 +0900

    review

--- semiproject/review.sql
+++ semiproject/review.sql
@@ -6,6 +6,6 @@ contents_no references contents(contents_no) on delete cascade,
 review_writer references client(client_id) on delete cascade,
 review_content varchar2(300) not null,
 review_time date default sysdate not null,
-review_score number not null check(review_score in(1,2,3,4,5))
+review_score number not null check(review_score in(1,2,3,4,5)) 
 
 );
