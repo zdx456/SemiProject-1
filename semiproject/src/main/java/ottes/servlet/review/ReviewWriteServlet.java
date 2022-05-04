7701bc6950604d35b05e2f5933d5996a983fe211
reeyuna <andyouna@hanmail.net>
2022-05-04 11:04:53 +0900

    1. mainpage logo image 업로드 2. 리뷰 작성 서블릿 생성 3. 리뷰 수정 서블릿 생성 4. 리뷰 삭제 서블릿 생성

---
+++ semiproject/src/main/java/ottes/servlet/review/ReviewWriteServlet.java
@@ -0,0 +1,45 @@
+package ottes.servlet.review;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ottes.beans.ReviewDao;
+import ottes.beans.ReviewDto;
+
+@WebServlet(urlPatterns = "/contents/review_insert.svt")
+//urlPatterns는 contents 파트와 협의 후 변경 가능
+
+public class ReviewWriteServlet extends HttpServlet{
+		
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			// 리뷰 번호 (reviewNo) +리뷰 내용(reviewContent) + 평점(reviewScore) + 컨텐츠 번호(contentsNo)
+			// + 작성자 (로그인한 HttpSession)
+			ReviewDto reviewDto = new ReviewDto();
+			reviewDto.setReviewNo(Integer.parseInt(req.getParameter("reviewNo")));
+			reviewDto.setReviewContent(req.getParameter("reviewContent"));
+			reviewDto.setReviewScore(Integer.parseInt(req.getParameter("reviewSccore")));
+			reviewDto.setContentsNo(Integer.parseInt(req.getParameter("ContentsNo")));
+			reviewDto.setReviewWriter((String)req.getSession().getAttribute("login"));
+			
+			ReviewDao reviewDao = new ReviewDao();
+			reviewDao.insert(reviewDto);
+			
+			// 리뷰 작성 후 컨텐츠 상세보기 페이지 그대로 돌아오기
+			resp.sendRedirect("detail.jsp?ContentsNo="+reviewDto.getContentsNo());
+			
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+	
+
+}
