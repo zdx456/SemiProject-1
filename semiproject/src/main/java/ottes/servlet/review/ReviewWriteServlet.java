4436c827204dd72aa319fa16de05eeea9edb674e
reeyuna <andyouna@hanmail.net>
2022-05-06 11:51:28 +0900

    ReviewDao, ReviewWriteServlet

--- semiproject/src/main/java/ottes/servlet/review/ReviewWriteServlet.java
+++ semiproject/src/main/java/ottes/servlet/review/ReviewWriteServlet.java
@@ -28,16 +28,16 @@ public class ReviewWriteServlet extends HttpServlet{
 			reviewDto.setReviewNo(reviewDao.getSequence());
 			
 			reviewDto.setReviewContent(req.getParameter("reviewContent"));
-			reviewDto.setReviewScore(Integer.parseInt(req.getParameter("reviewSccore")));
-			reviewDto.setContentsNo(Integer.parseInt(req.getParameter("ContentsNo")));
+			reviewDto.setReviewScore(Integer.parseInt(req.getParameter("reviewScore")));
+			reviewDto.setContentsNo(Integer.parseInt(req.getParameter("contentsNo")));
 			
 			// writer은 로그인한 세션에서 사용
-			reviewDto.setReviewWriter((String)req.getSession().getAttribute("login"));
+			// reviewDto.setReviewWriter((String)req.getSession().getAttribute("login"));
 			
 			reviewDao.insert(reviewDto);
 			
 			// 리뷰 작성 후 컨텐츠 상세보기 페이지 그대로 돌아오기
-			resp.sendRedirect("detail.jsp?ContentsNo="+reviewDto.getContentsNo());
+			resp.sendRedirect("detail.jsp?contentsNo="+reviewDto.getContentsNo());
 			
 					
 		} catch (Exception e) {
