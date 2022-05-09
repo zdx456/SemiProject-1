cd925ebc17a7f0330f0070d929acf1054038cc60
minyugyeong <alsdbrud123@naver.com>
2022-05-09 15:32:53 +0900

    ContentsActor Dao 메소드 추가 업로드

--- semiproject/src/main/java/ottes/beans/ContentsActorDao.java
+++ semiproject/src/main/java/ottes/beans/ContentsActorDao.java
@@ -23,5 +23,21 @@ public class ContentsActorDao {
 		
 		con.close();
 	}
+	
+	//삭제
+	public boolean delete(int contentsNo) throws Exception {
+		
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "delete contents_actor where contents_no = ?"; 
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, contentsNo);
+		int count = ps.executeUpdate();
+		
+		con.close();
+		
+		return count > 0;
+	}
 
 }
