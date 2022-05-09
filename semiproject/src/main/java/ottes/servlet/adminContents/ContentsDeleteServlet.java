679389890cd30ff9f0eb98c79e127a1bd8562fea
minyugyeong <alsdbrud123@naver.com>
2022-05-09 15:31:27 +0900

    콘텐츠 관리자 수정 서블릿 구현 완료

--- semiproject/src/main/java/ottes/servlet/adminContents/ContentsDeleteServlet.java
+++ semiproject/src/main/java/ottes/servlet/adminContents/ContentsDeleteServlet.java
@@ -8,7 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ottes.beans.ActorDao;
+import ottes.beans.AttachmentDao;
+import ottes.beans.ContentsActorDao;
+import ottes.beans.ContentsAttachmentDao;
 import ottes.beans.ContentsDao;
+import ottes.beans.OttContentsDao;
 
 @WebServlet(urlPatterns = "/adminContents/delete.svt")
 public class ContentsDeleteServlet extends HttpServlet {
@@ -20,16 +25,34 @@ public class ContentsDeleteServlet extends HttpServlet {
 			int contentsNo = Integer.parseInt(req.getParameter("contentsNo"));
 			
 			//처리
+			//(2) 배우 테이블 삭제
+			ActorDao actorDao = new ActorDao();
+			actorDao.delete(contentsNo);
+			
+			//(3) 컨텐츠-배우 테이블 삭제
+			ContentsActorDao contentsActorDao = new ContentsActorDao();
+			contentsActorDao.delete(contentsNo);
+			
+			//(4) 첨부파일 테이블 삭제
+			AttachmentDao attachmentDao = new AttachmentDao();
+			attachmentDao.delete(contentsNo);
+			
+			//(5) 컨텐츠-첨부파일 테이블 삭제
+			ContentsAttachmentDao contentsAttachmentDao = new ContentsAttachmentDao();
+			contentsAttachmentDao.delete(contentsNo);
+			
+			//(6) ott별 컨텐츠 테이블 삭제
+			OttContentsDao ottContentsDao = new OttContentsDao();
+			ottContentsDao.delete(contentsNo);
+			
+			//(1) 컨텐츠 테이블 삭제
 			ContentsDao contentsDao = new ContentsDao();
-			boolean isSuccess = contentsDao.delete(contentsNo);
+			contentsDao.delete(contentsNo);
+			
 			
 			//출력
-			if(isSuccess) {
-				resp.sendRedirect(req.getContextPath() + "/adminContents/list.jsp");
-			}
-			else {
-				resp.sendError(404);
-			}
+			resp.sendRedirect(req.getContextPath() + "/adminContents/list.jsp");
+
 			
 		}
 		catch(Exception e) {
