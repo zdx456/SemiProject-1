68e547fe01295f3262f2c9aa8597cc19f7d45fcb
reeyuna <andyouna@hanmail.net>
2022-05-10 17:42:24 +0900

    ottContentsDao에 OTT select method 추가

--- semiproject/src/main/java/ottes/beans/OttContentsDao.java
+++ semiproject/src/main/java/ottes/beans/OttContentsDao.java
@@ -39,6 +39,35 @@ public class OttContentsDao {
 	}
 	
 	
+public OttContentsDto selectOne(int contentsNo) throws Exception {
+		
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select * from ott_attachment O left outer join ott_contents C on O.ott_no = C.ott_no "
+				+ " left outer join contents B on B.contents_no = C.contents_no "
+				+ " where C.contents_no = ?";
+				
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, contentsNo);
+		ResultSet rs = ps.executeQuery();
+		
+		OttContentsDto ottContentsDto;
+		
+		if(rs.next()) {
+			ottContentsDto = new OttContentsDto();
+			ottContentsDto.setContentsNo(rs.getInt("contents_no"));
+			ottContentsDto.setOttNo(rs.getInt("ott_no"));
+
+		}
+		else {
+			ottContentsDto = null;
+		}
+		
+		con.close();
+		
+	return ottContentsDto;
+}
+	
 }
 
 
