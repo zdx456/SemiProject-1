84f0983da71ad9d20b20fa5698410a4d300d5961
zdx456 <zdx456@naver.com>
2022-05-13 19:48:18 +0900

    서블릿 수정 Ott 등록 수정

--- semiproject/src/main/java/ottes/servlet/clientOtt/ClientOttInsertServlet.java
+++ semiproject/src/main/java/ottes/servlet/clientOtt/ClientOttInsertServlet.java
@@ -19,14 +19,17 @@ public class ClientOttInsertServlet extends HttpServlet {
 		try {
 			
 			String clientId = (String)req.getSession().getAttribute("login");
+			ClientOttDao clientOttDao = new ClientOttDao();
 			
-			//배열로 받기! 체크박스 값 여러개 가능 하므로
+			boolean haveOtt = req.getParameter("ottNo") != null && !req.getParameter("ottNo").equals("");
+			
+			if(haveOtt) {
+				//배열로 받기! 체크박스 값 여러개 가능 하므로
 			String [] ottNoStr = req.getParameterValues("ottNo") ;
 			
 			// String 배열은 int 배열로 변환 (stream 이용)
 			int [] ottNoArray = Arrays.stream(ottNoStr).mapToInt(Integer::parseInt).toArray();
 			
-			ClientOttDao clientOttDao = new ClientOttDao();
 			
 			clientOttDao.delete(clientId);
 			
@@ -38,7 +41,11 @@ public class ClientOttInsertServlet extends HttpServlet {
 			
 			
 			resp.sendRedirect(req.getContextPath()+"/mypage/main.jsp");
-			
+			}
+			else {
+				clientOttDao.delete(clientId);
+				resp.sendRedirect(req.getContextPath()+"/mypage/main.jsp");
+			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
